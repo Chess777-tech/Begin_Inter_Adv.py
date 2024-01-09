398e7ca78f87705a1955a3ff16379f8e7b7f58c4
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2024-01-09 12:38:16 -0600

    Even_Odd.py

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,5 +1,9 @@
 Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+
+
+#Write a function that takes an integer as input and prints whether it is even or odd.
+  
 >>> def even_odd(x):
 ...     if x % 2 == 0:
 ...         return "Even"
