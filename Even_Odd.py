c574bf937ff2fe54471b2014324a7eb89b4ed16e
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2024-01-09 12:37:19 -0600

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,13 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def even_odd(x):
+...     if x % 2 == 0:
+...         return "Even"
+...     elif x % 2 == 1:
+...         return "Odd"
+... 
+...     
+>>> even_odd(1)
+'Odd'
+>>> even_odd(2)
+'Even'
