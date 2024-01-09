eea450a6dac98d146a9c816c9bd0a4ab0b39a0ae
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2024-01-09 12:28:38 -0600

    Add files via upload

---
+++ sum_two_nums.py
@@ -0,0 +1,14 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def two_num_sum(x,y):
+...     return x + y
+... 
+>>> two_num_sum(3,4)
+7
+>>> 
+>>> 
+>>> x = 1
+>>> y = 2
+>>> 
+>>> print(x+y)
+3
